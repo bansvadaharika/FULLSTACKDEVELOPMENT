1322a2deddc8d30e3a74e9df61099242f61ff78e
bansvadaharika <bansvadaharika@gmail.com>
2025-06-28 12:42:38 +0530

    unwanted

--- 28/06/25/SpliceAlgorithm.js
+++
@@ -1,9 +0,0 @@
-function frankenSplice(arr1, arr2, n) {
-  // Copy arr2 to avoid mutation
-  let result = arr2.slice();
-
-  // Use splice to insert arr1's elements starting at index n
-  result.splice(n, 0, ...arr1);
-
-  return result;
-}
